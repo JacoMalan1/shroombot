a124952299e86e2afd4b47e7d6bebd31ac100ee9
Jaco Malan <jacomalan.12@gmail.com>
2019-07-04 23:46:22 +0200

    Update

--- commands/joke.js
+++ commands/joke.js
@@ -5,7 +5,7 @@ async function callback(sender, args) {
     const api_url = 'https://official-joke-api.appspot.com/random_joke';
 
     const res = await fetch(api_url);
-    const data = res.json();
+    const data = await res.json();
     const response = data.setup + '\n\n' + data.punchline;
 
     return response;
