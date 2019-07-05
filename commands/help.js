58d9c9be7106430cd35ed9f2667143e6669d7276
Jaco Malan <jacomalan.12@gmail.com>
2019-07-05 13:34:14 +0200

    Update

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ const config = JSON.parse(fs.readFileSync('./assets/config.json'));
 
 async function callback(sender, args) {
     
-    const response = `List of commands for ${config.botname}:`;
+    let response = `List of commands for ${config.botname}:`;
     const commandList = JSON.parse(fs.readFileSync('./assets/commandlist.json')).commands;
 
     for (let cmd of commandList)
