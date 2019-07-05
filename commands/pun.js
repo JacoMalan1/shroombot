0e88655d8fb478a4ac07a39401e2dff19ccf19f4
Jaco Malan <jacomalan.12@gmail.com>
2019-07-05 12:30:45 +0200

    Update

--- commands/pun.js
+++ commands/pun.js
@@ -2,17 +2,7 @@ const fs = require('fs');
 
 async function callback(sender, args) {
 
-    fs.readdir('.', (err, files) => {
-
-        if (err) {
-            console.error(err);
-        } else {
-            console.log(files);
-        }
-
-    });
-
-    const fileContents = fs.readFileSync('./puns.json');
+    const fileContents = fs.readFileSync('./assets/puns.json');
     const puns = JSON.parse(fileContents).puns;
 
     return puns[Math.floor(Math.random() * puns.length)];
