6ec79efdff3a297bd4a232727dfe73b6ebc18067
Jaco Malan <jacomalan.12@gmail.com>
2019-07-05 13:30:52 +0200

    Update

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ const config = JSON.parse(fs.readFileSync('./assets/config.json'));
 async function callback(sender, args) {
     
     const response = `List of commands for ${config.botname}:`;
-    const commandList = JSON.parse(fs.readFileSync('./assets/config.json')).commands;
+    const commandList = JSON.parse(fs.readFileSync('./assets/commandlist.json')).commands;
 
     for (let cmd of commandList)
         response += `/${cmd.name} - ${cmd.description}\n`;
--- commands/kill.js
+++ commands/kill.js
@@ -1,9 +1,22 @@
 async function callback(sender, args) {
     
+    if (args.length < 1) {
+        return 'Not enough arguments!';
+    }
+
     const victor = sender.first_name;
     const victim = args[0];
 
-    return `${victor} beats ${victim} with a 440BX motherboard until they slump to the ground.`;
+    const stuff = '';
+    stuff.toLowerCase();
+
+    if (victim.toLowerCase() == 'franco') {
+        // Special detrimental case for the biggest idiot in the world
+        // :)
+        return `${victor} beats Franco with a 440BX motherboard until it slumps to the ground.`;
+    } else {
+        return `${victor} beats ${victim} with a 440BX motherboard until they slump to the ground.`;
+    }
 
 }
 
